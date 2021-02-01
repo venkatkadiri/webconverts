c8f40a9156b22b8bf4263731339045f08d28080e
venkatkadiri <venkatkadiri@hotmail.com>
2021-02-01 15:26:26 +0530

    2 nd comit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-const sections = document.querySelectorAll("section");
+/*const sections = document.querySelectorAll("section");
 const squre = document.querySelector(".squre");
+const Navbar = document.querySelector(".Navbar");
 const options = {
   threshold: 0.7,
 };
@@ -10,6 +11,13 @@ sections.forEach((section) => {
 
 function navCheck(entries) {
   entries.forEach((entry) => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page = ${className}]`);
+    const cordinates = activeAnchor.getBoundingClientRect();
+    const direction = {
+      height: cordinates.height,
+      width: cordinates.width,
+    };
     if (entry.isIntersecting) {
       let currentActive = document.querySelector(".Navbar a.active");
       if (currentActive) {
@@ -18,7 +26,12 @@ function navCheck(entries) {
       let newActive = document.querySelector(
         `.Navbar a[href="#${entry.target.getAttribute("id")}"]`
       );
+      const links = document.querySelector(".tabs");
+
       newActive.classList.add("active");
+      links.style.transform = `translateX(-${direction.width}px)`;
+
+      //Navbar.style.trasform = `translateX(-${direction.width})`;
     }
     if (window.innerWidth < 1434) {
       const newWidth = (window.innerWidth / 1434) * 100;
@@ -26,6 +39,47 @@ function navCheck(entries) {
       resposiveHeader.style.width = `${newWidth}%`;
       resposiveHeader.classList.add("overflow");
       resposiveHeader.style.overflow = "auto";
+    }
+  });
+}*/
+const sections = document.querySelectorAll("section");
+const squre = document.querySelector(".squre");
+
+const options = {
+  threshold: 0.7,
+};
+let observer = new IntersectionObserver(navCheck, options);
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+function navCheck(entries) {
+  entries.forEach((entry) => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page = ${className}]`);
+    const cordinates = activeAnchor.getBoundingClientRect();
+    const direction = {
+      height: cordinates.height,
+      width: cordinates.width,
+    };
+    if (entry.isIntersecting) {
+      var currentActive = document.querySelector(".Navbar a.active");
+      if (currentActive) {
+        currentActive.classList.remove("active");
+      }
+      let newActive = document.querySelector(
+        `.Navbar a[href="#${entry.target.getAttribute("id")}"]`
+      );
+      newActive.classList.add("active");
+
+      if (window.innerWidth < 1434) {
+        const newWidth = (window.innerWidth / 1434) * 100;
+        const resposiveHeader = document.querySelector("#tabs");
+
+        resposiveHeader.style.width = `${newWidth}%`;
+        resposiveHeader.classList.add("overflow");
+        resposiveHeader.style.overflow = "auto";
+      }
     }
   });
 }
